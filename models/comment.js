28ac7dfeb1c0f978ed006a69698d3c28681dce0c
Diane Lo <Diane@Dianes-MacBook-Pro.local>
2015-09-09 09:23:46 -0700

    author to comments

--- models/comment.js
+++ models/comment.js
@@ -5,7 +5,8 @@ var User = require("./user.js");
 
 var CommentSchema = new Schema({
 	createdAt: { type: Date, default: Date.now },
-	comments: {type: String, require: true}
+	comments: {type: String, require: true},
+	author: {type: String, require: true}
 });
 
 var Comment = mongoose.model("Comment", CommentSchema);
